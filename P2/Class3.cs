033e00949aa7fe6ae369f09614a3f1838ef9de1d
rolanzm <rolanzm@hotmail.com>
2016-12-22 20:07:03 -0500

    asd

---
+++ P2/Class3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    class Class3
+    {
+        public DateTime MyProperty { get; set; }
+    }
+}
